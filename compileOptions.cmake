af884f6775720d0794237a9d09538160c65fb8b1
gonzalodm <gonzalodm20@gmail.com>
2023-09-07 11:18:17 +0200

    Add cmake compilations: only cpu

---
+++ compileOptions.cmake
@@ -0,0 +1,14 @@
+# Compile Options
+option(CPU "LIO will be compiled using CPUs only" ON)
+option(GPU "LIO will be compiled using GPUs and CPUs" OFF)
+option(EXTERNAL "LIO will be compiled using Libxc and Libint libraries" OFF)
+option(DOUBLE "LIO will be compiled using Double Precision" OFF)
+option(WARNING "LIO will be compiled using Warning Flags" OFF)
+
+if (GPU) 
+   message(FATAL_ERROR "For the moment no GPU compilations is provided in cmake")
+endif()
+
+#if (EXTERNAL)
+#   message(FATAL_ERROR "For the moment usage of external components are not allowed")
+#endif()
