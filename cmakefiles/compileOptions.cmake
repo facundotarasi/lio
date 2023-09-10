3d4db4186491c1f0db7879b2921ef68b9d709011
gonzalodm <gonzalodm20@gmail.com>
2023-09-10 11:56:58 +0200

    Add options to download libxc and libint. I also reformated the cmake code

--- cmakefiles/compileOptions.cmake
+++ cmakefiles/compileOptions.cmake
@@ -2,6 +2,8 @@
 option(CPU "LIO will be compiled using CPUs only" ON)
 option(GPU "LIO will be compiled using GPUs and CPUs" OFF)
 option(EXTERNAL "LIO will be compiled using Libxc and Libint libraries" OFF)
+option(COMPILE_EXTERNAL "LIO will compile libxc and libint and then 
+                         will make the link with LIO" OFF)
 option(DOUBLE "LIO will be compiled using Double Precision" OFF)
 option(WARNING "LIO will be compiled using Warning Flags" OFF)
 
